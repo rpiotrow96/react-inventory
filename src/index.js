6110adff706cf32e57ffe3708ad249c8ccb74fd9
rpiotrow <rpiotrow@gmail.com>
2021-11-16 21:32:06 +0100

    changed bootstap version to 4.6.1 to make modals work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 import App from "./App";
 import itemsJson from "./items.json";
 
