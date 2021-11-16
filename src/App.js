3e4fc57ed98db62c4618c76ad9c83ed6d6a08f1d
rpiotrow <rpiotrow@gmail.com>
2021-11-16 15:39:08 +0100

    refactoring, removed hook

--- src/App.js
+++ src/App.js
@@ -3,31 +3,98 @@ import React from 'react';
 import ItemsTable from './ItemsTable'
 import Filters from './Filters'
 
-class App extends React.Component {
+export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            maxValue: null,
-            minValue: null
+            items: props.items,
+            sortedColumn: 'id',
+            direction: 'ascending',
+
+            minValue: null,
+            maxValue: null
         }
     }
-    setMinValue = (childData) => {
-        this.setState({minValue: childData});
+
+    setMinValue = (value) => {
+        if (value === '' || value === null) {
+            value = 0;
+        }
+        this.setState({
+            minValue: value
+        }, () => {
+            this.filterItems();
+        });
     }
 
-    setMaxValue = (childData) => {
-        this.setState({maxValue: childData});
+    setMaxValue = (value) => {
+        if (value === '' || value === null) {
+            value = Number.POSITIVE_INFINITY;
+        }
+        this.setState({
+            maxValue: value
+        }, () => {
+            this.filterItems();
+        });
+    }
+
+    requestSort = key => {
+        let direction = 'ascending';
+        if (this.state.sortedColumn === key && this.state.direction === 'ascending') {
+            direction = 'descending';
+        }
+        this.setState({
+            sortedColumn: key,
+            direction: direction
+        }, () => {
+            this.sortItems();
+        });
+    }
+
+    sortItems = () => {
+        let sortedItems = [...this.state.items];
+        if (this.state.sortedColumn !== null) {
+            sortedItems.sort((a, b) => {
+                let column = this.state.sortedColumn;
+                if (a[column] < b[column]) {
+                    return this.state.direction === 'ascending' ? -1 : 1;
+                }
+                if (a[column] > b[column]) {
+                    return this.state.direction === 'ascending' ? 1 : -1;
+                }
+                return 0;
+            });
+        }
+
+        this.setState({
+            items: sortedItems
+        });
+    }
+
+    filterItems = () => {
+        this.sortItems();
+        let filteredItems = [...this.props.items];
+        filteredItems = filteredItems.filter(item => item.price >= this.state.minValue && item.price <= this.state.maxValue);
+        this.setState({
+            items: filteredItems
+        })
+    }
+
+    getClassNamesFor = (name) => {
+        return this.state.sortedColumn === name ? this.state.direction : undefined;
     }
 
     render() {
         return (
             <div className="container pt-3">
                 <Filters setMinValue = {this.setMinValue} setMaxValue = {this.setMaxValue}/>
-                <ItemsTable items ={this.props.items} maxValue={this.state.maxValue} minValue={this.state.minValue}/>
+                <ItemsTable
+                    items ={this.state.items}
+                    requestSort={this.requestSort}
+                    sortItems={this.sortItems}
+                    getClassNamesFor={this.getClassNamesFor}
+                />
             </div>
         );
     }
-
 }
-
-export default App;
--- src/ItemsTable.js
+++ src/ItemsTable.js
@@ -1,106 +1,61 @@
 import React from "react";
 
-const useSortedItems = (items, config = null) => {
-    const [sortConfig, setSortConfig] = React.useState(config);
+export default class ItemsTable extends React.Component{
 
-    const sortedItems = React.useMemo(() => {
-        let sortableItems = [...items];
-        if (sortConfig !== null) {
-            sortableItems.sort((a, b) => {
-                if (a[sortConfig.key] < b[sortConfig.key]) {
-                    return sortConfig.direction === 'ascending' ? -1 : 1;
-                }
-                if (a[sortConfig.key] > b[sortConfig.key]) {
-                    return sortConfig.direction === 'ascending' ? 1 : -1;
-                }
-                return 0;
-            });
-        }
-        return sortableItems;
-    }, [items, sortConfig]);
-
-    const requestSort = key => {
-        let direction = 'ascending';
-        if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
-            direction = 'descending';
-        }
-        setSortConfig({ key, direction });
-    }
-
-    return { items: sortedItems, requestSort, sortConfig };
-}
-
-function ItemsTable(props) {
-
-    const newItems = props.items;
-    let minValue = props.minValue;
-    let maxValue = props.maxValue;
-    const { items, requestSort, sortConfig } = useSortedItems(newItems);
-
-    if (minValue === '' || minValue === null) {
-        minValue = 0;
-    }
-    if (maxValue === '' || maxValue === null) {
-        maxValue = Number.POSITIVE_INFINITY;
-    }
-    const filteredItems  = items.filter(o => o.price >= minValue && o.price <= maxValue);
-
-    const getClassNamesFor = (name) => {
-        if (!sortConfig) {
-            return;
-        }
-        return sortConfig.key === name ? sortConfig.direction : undefined;
-    };
-
-    return (
-        <table className="table table-striped table-dark">
-        <thead>
-        <tr>
-            <th>
-                <button type="button" onClick={() => requestSort('id')} className={`btn btn-light btn-sm ${getClassNamesFor('id')}`}>
-                    ID
-                </button>
-            </th>
-            <th>
-                <button type="button" onClick={() => requestSort('name')} className={`btn btn-light btn-sm ${getClassNamesFor('name')}`}>
-                    Nazwa
-                </button>
-            </th>
-            <th>
-                <button type="button" onClick={() => requestSort('price')} className={`btn btn-light btn-sm ${getClassNamesFor('price')}`}>
-                    Cena
-                </button>
-            </th>
-            <th>
-                <button type="button" onClick={() => requestSort('producer')} className={`btn btn-light btn-sm ${getClassNamesFor('producer')}`}>
-                    Producent
-                </button>
-            </th>
-            <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        {
-            filteredItems.map(product => {
-            return (
-                <tr key={product.id}>
-                    <td>{product.id}</td>
-                    <td>{product.name}</td>
-                    <td>{product.price} zł</td>
-                    <td>{product.producer}</td>
-                    <td>
-                        <div className="btn-group">
-                            <button className="btn btn-outline-primary">Pokaż</button>
-                            <button className="btn btn-outline-success">Edytuj</button>
-                            <button className="btn btn-outline-danger">Usuń</button>
-                        </div>
-                    </td>
+    render()
+    {
+        return (
+            <table className="table table-striped table-dark">
+                <thead>
+                <tr>
+                    <th>
+                        <button type="button" onClick={() => this.props.requestSort('id')}
+                                className={`btn btn-light btn-sm ${this.props.getClassNamesFor('id')}`}>
+                            ID
+                        </button>
+                    </th>
+                    <th>
+                        <button type="button" onClick={() => this.props.requestSort('name')}
+                                className={`btn btn-light btn-sm ${this.props.getClassNamesFor('name')}`}>
+                            Nazwa
+                        </button>
+                    </th>
+                    <th>
+                        <button type="button" onClick={() => this.props.requestSort('price')}
+                                className={`btn btn-light btn-sm ${this.props.getClassNamesFor('price')}`}>
+                            Cena
+                        </button>
+                    </th>
+                    <th>
+                        <button type="button" onClick={() => this.props.requestSort('producer')}
+                                className={`btn btn-light btn-sm ${this.props.getClassNamesFor('producer')}`}>
+                            Producent
+                        </button>
+                    </th>
+                    <th></th>
                 </tr>
-            );
-        })}
-        </tbody>
-    </table>
-    );
+                </thead>
+                <tbody>
+                {
+                    this.props.items.map(product => {
+                        return (
+                            <tr key={product.id}>
+                                <td>{product.id}</td>
+                                <td>{product.name}</td>
+                                <td>{product.price} zł</td>
+                                <td>{product.producer}</td>
+                                <td>
+                                    <div className="btn-group">
+                                        <button className="btn btn-outline-primary">Pokaż</button>
+                                        <button className="btn btn-outline-success">Edytuj</button>
+                                        <button className="btn btn-outline-danger">Usuń</button>
+                                    </div>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        );
+    }
 }
-
-export default ItemsTable;
